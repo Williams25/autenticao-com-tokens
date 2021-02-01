cac8fdac8e8385c7befd30051777ec597daf6e1b
Williams25 <william.souza.timao@hotmail.com>
2021-02-01 14:43:26 -0300

    Adicionando body-parser

--- app.js
+++ app.js
@@ -2,11 +2,8 @@ const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
 
-app.use(
-  bodyParser.urlencoded({
-    extended: true
-  })
-);
-app.use(express.json())
+const { estrategiasAutenticacao } = require('./src/usuarios');
+
+app.use(bodyParser.json());
 
 module.exports = app;
